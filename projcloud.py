d5b73b36d4666d70cd8f8f9cdfb4b9be4dcdf344
Mohammed Saleh <msaleh@cs.ualberta.ca>
2022-11-01 11:55:02 -0600

    Split into usermenu

--- projcloud.py
+++ projcloud.py
@@ -1,7 +1,7 @@
 import sqlite3
 import sys
 from getpass import getpass
-import time
+from usermenu import userMenu
 
 import MySQLdb
 
@@ -150,61 +150,11 @@ def loginScreen():
         else:
             print("Select a valid option ! ! !")
 
-def addSession(uid):
-    global connection, cursor
-    cur_date = time.strftime("%Y-%m-%d %H:%M:%S")
-    t = (str(uid),)
-    cursor.execute('SELECT MAX(sno+0) FROM sessions WHERE uid=?;', t)
-    sno = cursor.fetchone()
-    if (sno[0] is None):
-        sno = 1
-    else:
-        sno = sno[0] + 1
-    cursor.execute('INSERT INTO sessions VALUES (?,?,?,?)', (uid, sno, cur_date, None))
-    connection.commit()
-    return sno
-
-def endSession(uid, sno):
-    global connection, cursor
-    end_date = time.strftime("%Y-%m-%d %H:%M:%S")
-    cursor.execute('UPDATE sessions SET end = ? WHERE uid = ? AND sno = ?', (end_date, uid, sno))
-    connection.commit()
-
-def userMenu(uid):
-    print("")
-    print("USER MENU")
-    print("---------")
-    print("Welcome ! What would you like to do?")
-    sno = None
-    while (1):
-        print("1: Start a listening session !")
-        print("2: Search for songs and playlists !")
-        print("3: Search for artists !")
-        print("4: End your session !")
-        print("5: Logout !")
-        x = input()
-        if (x not in ['1','2','3','4','5']):
-            print("Please put a valid input !")
-            continue
-        if (x == '5'):
-            print("Logging out !")
-            break
-        elif (x == '1'):
-            print('Starting a session !')
-            if (sno is not None):
-                print("You're already listening ! End this session first !")
-                continue
-            sno = addSession(uid)
-        elif (x == '4'):
-            if (sno is None):
-                print("You don't have a session !")
-                continue
-            else:
-                endSession(uid, sno)
-                sno = None
-
 def main():
     global connection, cursor
+    if (len(sys.argv) != 2):
+        print("Input the database !")
+        return
     db = "./" + str(sys.argv[1])
     connect(db)
     while (1):
@@ -223,7 +173,7 @@ def main():
 
         #User Menu
         if (x[0] == 1):
-            userMenu(x[1])
+            userMenu(x[1], connection, cursor)
         else:
             print("")
 
